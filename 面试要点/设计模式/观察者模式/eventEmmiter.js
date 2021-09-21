c38c1fe9b97cc6bfca4d8f6195b7c5d2171b409b
wangeastsea <wangeastsea@icloud.com>
2021-09-21 18:33:01 +0800

    观察者模式总结

---
+++ 面试要点/设计模式/观察者模式/eventEmmiter.js
@@ -0,0 +1,46 @@
+// 全局时间总线的特点
+
+// 全局事件总线的特点——所有事件的发布/订阅操作，必须经由事件中心，禁止一切“私下交易”！
+
+
+/**
+ *  this.bus.$on('someEvent', func)
+    this.bus.$emit('someEvent', params)
+ */
+
+class EventEmitter {
+    constructor() {
+        this.eventManage = {}
+    }
+    // 触发监听事件
+    emit(fnName, ...params) {    
+        if (!this.eventManage[fnName]) return 
+        const eventManage = this.eventManage[fnName].slice()
+        eventManage.forEach((cb) => {
+            cb(...params)
+        })
+    }
+    off(fnName, cb) {
+        if (!this.eventManage[fnName]) return 
+        let callbacks = this.eventManage[fnName]
+        let index = callbacks.indexOf(cb)
+        if (index !== -1)  {
+            callbacks.splice(index, 1)
+        }
+    }
+    // 注册监听事件
+    on(fnName, cb) {
+        if (!this.eventManage[fnName]) {
+            this.eventManage[fnName] = []
+        } 
+        this.eventManage[fnName].push(cb)
+    }
+    once(fnName, cb) {
+        let middleCb = (...params) => {
+            fnName(...params)
+            this.off(fnName, middleCb)
+        }
+        this.on(fnName, middleCb)
+    }
+}
+
